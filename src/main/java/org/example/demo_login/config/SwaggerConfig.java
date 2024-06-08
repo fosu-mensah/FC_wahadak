2cbdee29b82a03a53a1994d0face462bb350c2bf
Kim Youngjae <minorgeniuss@naver.com>
2024-06-08 20:20:10 +0900

    MyBatisCofig 파일 설정 추가 + CaseInsensitiveEumTypeHandler 파일 추가 + application.propertis에 Encoding 설정 추가

--- src/main/java/org/example/demo_login/config/SwaggerConfig.java
+++ src/main/java/org/example/demo_login/config/SwaggerConfig.java
@@ -26,19 +26,19 @@ public class SwaggerConfig {
                 .bearerFormat("JWT")
         );
 
-        // OpenAPI 객체를 반환합니다. 이 객체는 API 문서의 전반적인 구성을 정의합니다.
+        // OpenAPI 객체를 반환. 이 객체는 API 문서의 전반적인 구성을 정의한다.
         return new OpenAPI()
-                .components(new Components())  // 빈 Components 객체를 추가합니다.
-                .info(apiInfo())  // API 정보(제목, 설명, 버전)를 설정합니다.
-                .addSecurityItem(securityRequirement)  // 보안 요구 사항을 추가합니다.
-                .components(components);  // 구성 요소(보안 스키마 등)를 추가합니다.
+                .components(new Components())  // 빈 Components 객체를 추가.
+                .info(apiInfo())  // API 정보(제목, 설명, 버전)를 설정.
+                .addSecurityItem(securityRequirement)  // 보안 요구 사항을 추가.
+                .components(components);  // 구성 요소(보안 스키마 등)를 추가.
     }
 
     private Info apiInfo() {
-        // Info 객체는 API 문서의 제목, 설명 및 버전을 정의합니다.
+        // Info 객체는 API 문서의 제목, 설명 및 버전을 정의한다.
         return new Info()
-                .title("FC wahadak API Test")  // API 문서의 제목을 설정합니다.
-                .description("FC wahadak Swagger")  // API 문서에 대한 설명을 설정합니다.
-                .version("1.0.0");  // API 문서의 버전을 설정합니다.
+                .title("FC wahadak API Test")  // API 문서의 제목 설정.
+                .description("FC wahadak Swagger")  // API 문서에 대한 설명 설정.
+                .version("1.0.0");  // API 문서의 버전 설정.
     }
 }
